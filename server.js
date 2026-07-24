bcb0f7bb48513d92506bb502eb5b01579509dff2
Kitesa Merga <kitesamerga2025@gmail.com>
2026-07-24 15:17:26 +0300

    CORS URL slash haqi

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ connectDB();
 const app = express();
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://student-management-system-client-red.vercel.app/', // 
+  credentials: true
+}));
 app.use(express.json());
 
 // routes
